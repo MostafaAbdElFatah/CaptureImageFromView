329eb35aaa0a86b4819ed8cc022dc7ef8d288fd4
Mostafa AbdEl Fatah <mostafaabdelfatah@Mostafas-Mac.local>
2018-10-17 14:44:39 -0700

    first commit

--- TackeScreenshot/ViewController.swift
+++ TackeScreenshot/ViewController.swift
@@ -15,11 +15,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func takeScreenShot(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        self.view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
     }
-
+    
 
 }
 
